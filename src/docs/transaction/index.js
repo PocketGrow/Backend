af8d7b389cb11ba0c4ab9c15506b13c4882b3bdb
aenq <rachelmartam@gmail.com>
2023-12-18 22:40:24 +0700

    update swagger

--- src/docs/transaction/index.js
+++ src/docs/transaction/index.js
@@ -2,6 +2,7 @@ const getTransaction = require("./getTransaction");
 const getTransactionById = require("./getTransactionById");
 const createTransaction = require("./createTransaction");
 const deleteTransaction = require("./deleteTransaction");
+const getLatestTransaction = require("./getLatestTransaction");
 
 module.exports = {
   "/api/transaction": {
@@ -11,5 +12,8 @@ module.exports = {
   "/api/transaction/{transactionId}": {
     ...getTransactionById,
     ...deleteTransaction,
+  },
+  "/api/transaction/latest": {
+    ...getLatestTransaction,
   },
 };
